dde4a5291cf729cf2dceeaee00716be074f477bc
ybuoro <119375735+ybuoro@users.noreply.github.com>
2022-12-20 18:20:56 +0100

    Update script.R

--- script.R
+++ script.R
@@ -3450,6 +3450,49 @@ capture.output(list17, file="/home/yoann/Bureau/PAIN TSIMANE REVIEWER VERSION/TA
 
 ######### FIGURE S8 #########
 
+dddata=data[!data$cause.work=="NA",]
+dddata=dddata[!dddata$cause.work=="Nonspecific",] # excluding rows that have "Nonspecific" as a work-related cause of pain
+dddata$cause.work<-factor(dddata$cause.work, levels = c("Horticulture", "Overload", "Hunt or forage", "Transport", "Wood", "Fish", "Domestic")) # specifying the order of priority for the categories of cause.work for later plotting
+
+tdlab=dddata[!is.na(dddata$YearsSchool),]
+
+## 1) compute number of person-observations for each age category ##
+tdlab$YearsSchool<-as.character(tdlab$YearsSchool)
+
+tdlab[tdlab$YearsSchool=="0.083333333",72]<-"0"
+tdlab[tdlab$YearsSchool=="0.666666667",72]<-"1"
+tdlab[tdlab$YearsSchool=="0.333333333",72]<-"0"
+tdlab[tdlab$YearsSchool=="7",72]<-"7+"
+tdlab[tdlab$YearsSchool=="9",72]<-"7+"
+tdlab[tdlab$YearsSchool=="13",72]<-"7+"
+
+unique(dlabdata$YearsSchool)
+# storing the correct order of each age category text in order to display it on the figure 
+vec=c("0", "1", "2", "3", "4", "5", "6", "7+")
+dlabdata=tdlab
+dlabdata$YearsSchool<-factor(dlabdata$YearsSchool, levels=vec)
+## ----------- ##
+
+## 2) coding of the plot ##
+# specifying colours for each category of work
+colfill2 <- c("Overload" = "#3B4992FF",  "Hunt or forage" = "#EE0000FF", "Horticulture" = "#008B45FF", "Transport" = "#631879FF", "Fish" = "lightgoldenrod1", "Domestic" = "chocolate1", "Wood"="#008280FF")
+
+fig.S8 <- ggplot(dlabdata, aes(x = YearsSchool)) +
+  theme_bw()+
+  geom_bar(aes(fill = cause.work), position = "fill",  width=0.6) +
+  theme(legend.position = "right")+
+  labs(fill="Attributions")+
+  ylab("Percent of work-related pain attributions") +
+  xlab("Years of schooling") +
+  theme(text = element_text(size=55),plot.title = element_text(hjust = 0.5), panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  scale_fill_manual(values = colfill2)+
+  scale_y_continuous(labels = scales::percent)
+
+
+fig.S8
+dev.print(tiff, "/home/yoann/Bureau/PAIN TSIMANE REVIEWER VERSION/FIGURES/figureS8.tiff", width=1000, height=1200, units="px")
+dev.off()
+
 ######### --------------------------------------------------------------------------------------------------- ######### 
 
 
